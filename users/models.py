75ad25933c2031440650ba30cd27c12aba9169ed
smatura001 <smatura@nms.gov.bz>
2026-08-11 12:20:42 -0600

    Inventory - Update assigned user display

--- users/models.py
+++ users/models.py
@@ -16,7 +16,6 @@ class Employee(models.Model):
 
     first_name  = models.CharField(max_length=20, blank=True)
     last_name   = models.CharField(max_length=20, blank=True)
-
     email       = models.CharField(max_length=60, blank=True)
     phone       = models.CharField(max_length=20, blank=True)
     department  = models.ForeignKey("system_core.DepartmentSection", on_delete=models.SET_NULL, blank=True, null=True)
@@ -25,8 +24,7 @@ class Employee(models.Model):
 
     #photo       = models.ImageField(upload_to="images/users/profile", blank=True, null=True)
     has_user_account = models.BooleanField(default=False)
-    #user        = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, blank=True, null=True)
     user        = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True,blank=True)
     
     def __str__(self):
-        return self.user.first_name
+        return f"" + self.first_name + " " + self.last_name
