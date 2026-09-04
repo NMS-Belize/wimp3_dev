1786551561e8aa0a8dfcd7eb36796af157931937
smatura001 <smatura@nms.gov.bz>
2026-09-04 08:09:49 -0600

    Update logo on login screen; add logout button to header buttons

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 
+from company.models import Company
+
 class UserProfile(models.Model):
     user        = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     phone       = models.CharField(max_length=20, blank=True)
@@ -8,6 +10,8 @@ class UserProfile(models.Model):
     office_location = models.ForeignKey("system_core.OfficeLocation", on_delete=models.SET_NULL, blank=True, null=True)
     job_title   = models.ForeignKey("system_core.JobTitle", on_delete=models.SET_NULL, blank=True, null=True)
     photo       = models.ImageField(upload_to="images/users/profile", blank=True, null=True)
+
+    company = models.ForeignKey("company.Company",on_delete=models.SET_NULL,null=True,blank=True,related_name='users')
 
     def __str__(self):
         return self.user.username
